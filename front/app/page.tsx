1666a1035a5b75ddcc0ecede4e888906d1538fb4
Gyanthakur <gps.96169@gmail.com>
2024-06-09 23:56:09 +0530

    feat:list added

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,13 +1,19 @@
+"use client"
 import Banner from "@/components/Landing/Banner";
 import Footer from "@/components/Landing/Footer";
 import Header from "@/components/Landing/Header";
 import TopSheets from "@/components/Landing/TopSheets";
+// import List from "@/components/List";
+import CreateSheet from "@/components/Manage/CreateSheet";
+import List from "@/components/Sheets/List";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen overflow-hidden w-full flex-col items-center gap-4 sm:px-8 px-4">
       <Banner />
       <TopSheets />
+      <List />
+      
     </main>
   );
 }
